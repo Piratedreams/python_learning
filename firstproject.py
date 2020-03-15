28e914d2e6e127ef35d8e61e568698d74456dd7f
Piratedreams <wall.benjamina@gmail.com>
2020-03-14 18:27:24 -0600

    created function and loop

--- firstproject.py
+++ firstproject.py
@@ -1,11 +1,48 @@
 
 
 
+# My attempt at it
+
+
+# What will be filled out in the print section, empty strings
+phrase = ['']
 
 
 
 # Create input of say something
-
+def say_something(input):
+    something = input('say something: ')
+    while something in input:
+        if input != 'done':
+            phrase = phrase + something
+        else:
 # print out statement of whats  input
+            print(phrase)
+        
 
-# add a break statement when user is finished.
+
+
+
+
+# Instructor solution
+def sentence_maker(phrase):
+    interrogatives = {'how', 'what', 'why'}
+    capitalized = phrase.capitalize()
+    if phrase.startswith(interrogatives):
+        return "{}?".format(capitalized)
+    else:
+        return "{}.".format(capitalized)
+
+
+
+results = []
+
+while True:
+    user_input = input('say something: ')
+    if user_input == '\end':
+        break
+    else:
+        results.append(user_input)
+
+
+print(results)
