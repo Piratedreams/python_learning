77262023085bb0c1cde4e794a1c8bf41e78bfe4c
Piratedreams <wall.benjamina@gmail.com>
2020-03-17 10:02:16 -0600

    creating functions with arbitrary numbers

--- temp.py
+++ temp.py
@@ -1,6 +1,11 @@
 temps = [221, 234, 340, -999, 230]
 
-new_temps = [temp/ 10 for temp in temps if temp != -999]
+
+
+# if for loop needs to be in a list, it goes at the end.
+new_temps = [temp / 10 if temp != -999 else 0 for temp in temps]
 
 
 print(new_temps)
+
+
