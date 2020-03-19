0490a535909390048d57ecac5ab179e202ce53ae
Piratedreams <wall.benjamina@gmail.com>
2020-03-19 13:55:17 -0600

    wrote vegetables

--- nkwa.py
+++ nkwa.py
@@ -1,13 +1,9 @@
-myfile =  open("fruits.txt")
 
-content = myfile.read()
-myfile.close()
 
 
 # opening files using with
-with open('files/vegetables.txt') as myfile:
-    myfile.wrtie('tomato')
+with open('files/vegetables.txt', 'w') as myfile:
+    myfile.write('tomato')
 
-print(content)
 
 
