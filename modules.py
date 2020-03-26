43ff079e7d1450b9fd34a10dd8eed82a621bbf4f
Piratedreams <wall.benjamina@gmail.com>
2020-03-25 19:15:52 -0600

    added json and app data for first application

--- modules.py
+++ modules.py
@@ -1,11 +1,12 @@
 import time
 import os
-
+import pandas
 
 while True:
-    if os.path.exists('files/vegetables.txt'):
-        with open('files/vegetables.txt') as file:
-            print(file.read())
+    if os.path.exists('files/temps_today.csv'):
+        data = pandas.read_csv('files/temp_today.csv')
+        # print(data.mean()[st1]) for only station 1
+        print(data.mean())
     else:
         print('file does not exist')
         time.sleep(10)
